191f9849d9a69ce7000c54e7f52161ad4c7dcad2
ElzarMachado <101478824+ElzarMachado@users.noreply.github.com>
2022-05-14 12:08:30 -0700

    metodo $"

--- EjemplosED2/Program.cs
+++ EjemplosED2/Program.cs
@@ -1,2 +1,31 @@
-﻿string nombre = " Elzar Machado ,";
-Console.WriteLine(nombre.Trim());
+﻿//string nombre = "Coca";
+//Console.WriteLine(nombre.Length);
+
+//if (string.IsNullOrWhiteSpace(nombre))
+//{
+//    Console.WriteLine("string vacio");
+//}
+//else
+//{
+//    Console.WriteLine(nombre);
+//}
+
+//int numero = 9;
+//string numeroConCeros = "9";
+// numeroConCeros = numeroConCeros.PadLeft(3, '0');
+//Console.WriteLine(numeroConCeros);
+
+string nombre = "Elzar";
+string apellido = "Machado";
+int edad = 20;
+
+//string NombreYedad =  nombre + | apellido + | edad;
+//Console.WriteLine(NombreYedad);
+
+//String NombreYedad = string.Format("{0}, {1}, {2}"), nombre, apellido, edad);
+//Console.WriteLine(NombreYedad);
+
+//string NombreYedad = $"{nombre} | {apellido} | {edad}";
+//    Console.WriteLine(NombreYedad);
+
+//Investigar concat
