50582ca86ba122bbc66dede5c9e385565b1802f1
ElzarMachado <101478824+ElzarMachado@users.noreply.github.com>
2022-05-14 13:13:31 -0700

    falto

--- EjemplosED2/Program.cs
+++ EjemplosED2/Program.cs
@@ -15,9 +15,9 @@
 // numeroConCeros = numeroConCeros.PadLeft(3, '0');
 //Console.WriteLine(numeroConCeros);
 
-string nombre = "Elzar";
-string apellido = "Machado";
-int edad = 20;
+//string nombre = "Elzar";
+//string apellido = "Machado";
+//int edad = 20;
 
 //string NombreYedad =  nombre + | apellido + | edad;
 //Console.WriteLine(NombreYedad);
@@ -29,3 +29,18 @@ int edad = 20;
 //    Console.WriteLine(NombreYedad);
 
 //Investigar concat
+
+//string numeroString = "5 x";
+//// ((((int numeroInt = Convert.ToInt32(numeroString);))))
+
+//int numeroInt = 0;
+//bool pudo = int.TryParse(numeroString, out numeroInt);
+//Console.WriteLine(pudo);
+
+string cadena = "Hola mundo Jose";
+string [] text = cadena.Split('|', ' ', )ToList();
+foreach(string s in text)
+{
+    Console.WriteLine(s);
+}
+//Console.WriteLine(cadena.IndexOf(" "));
